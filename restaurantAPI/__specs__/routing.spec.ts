19771882ee6f2b19682aa8f279fe79b6bd5e09ed
Boken Lin <bl@bokenlin.com>
2022-07-12 13:40:18 +0000

    got jest to work

---
+++ restaurantAPI/__specs__/routing.spec.ts
@@ -0,0 +1,16 @@
+import app from '../src/index';
+import request from 'supertest'
+
+// const app = require('../dist/index.bundle');
+// const request = require('supertest');
+
+describe("GET / ", () => {
+	test("Should be 200", async () => {
+		const response = await request(app).get("/");
+		expect(response.statusCode).toBe(200);
+	});
+});
+
+test("dummy", () => {
+	expect(2 + 2).toEqual(4)
+})
--- restaurantAPI/jest.config.ts
+++ restaurantAPI/jest.config.ts
@@ -1,14 +1,20 @@
-import type {Config} from '@jest/types';
+import type { Config } from '@jest/types';
+// import type { InitialOptionsTsJest } from 'ts-jest';
+// import { defaults as tsjPreset } from 'ts-jest/presets';
 
 // Sync object
 
 export default {
-    verbose:true,
-    moduleDirectories: ["node_modules", "src"],
-    globals: {
-        "ts-jest": {
-          isolatedModules: true,
-        },
-    },
-    transform:{}
-  };
+	verbose: true,
+	moduleDirectories: ["node_modules", "src"],
+	globals: {
+		"ts-jest": {
+			isolatedModules: true,
+		},
+	},
+	transform: {
+		'^.+\\.(ts|tsx)$': 'ts-jest',
+	},
+	preset: "ts-jest",
+	testEnvironment: 'node',
+};
